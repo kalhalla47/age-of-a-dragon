c6e32fd1530a42eee7a8ed2deda494f42effd18c
Kyle Hall <hallkd47@gmail.com>
2016-05-31 23:12:34 -0500

    adds prototype for Boot state

--- game/scripts/Boot.js
+++ game/scripts/Boot.js
@@ -1,2 +1,15 @@
 
 var AgeOfADragon = AgeOfADragon || {};
+
+AgeOfADragon.Boot = function() {};
+
+AgeOfADragon.Boot.prototype = {
+  preload: function() {
+
+  },
+
+  create: function() {
+    this.game.physics.startSystem(Phaser.Physics.ARCADE);
+    this.state.start('Preload');
+  }
+};
